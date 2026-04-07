f341fe552cbbbe383cbb31f371dcc194979d1442
Kancharlapalli Varshita <153096956+varshita94@users.noreply.github.com>
2026-04-07 16:28:13 +0530

    Time: 1 ms (99.98%), Space: 77.1 MB (87%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,18 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       //kadane's algorithm
+        int currSum = 0;
+        int maxSum = nums[0];
 
-       int currentSum = nums[0];
-       int maxSum = nums[0];
-        int n= nums.length;
-       for(int i=1 ;i<n;i++)
-       {
-        currentSum = Math.max(nums[i], currentSum+nums[i]);  //remove adding with the negatives
-        maxSum = Math.max(maxSum,currentSum);
+        for(int n : nums)
+        {
+            currSum = Math.max(currSum,0); //if the element is -ve then take 0
+            currSum += n;
+            maxSum = Math.max(maxSum, currSum);
 
-       }
+        }
 
-       return maxSum;
+        return maxSum;
 
     }
 }
