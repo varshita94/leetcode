bd6c1c8457c6a83fcb5eed0ff95b7a8f3a95ff5a
Kancharlapalli Varshita <153096956+varshita94@users.noreply.github.com>
2026-07-07 16:46:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (21.92%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    public int countOdds(int low, int high) {
+        //we can simply use a loop -> time limit exceeded
+
+        return( (high + 1) / 2 )- (low / 2); 
+
+        
+
+        
+    }
+}
